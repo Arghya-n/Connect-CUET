cbeffe74ce5f3d008a7efe3c1b3ff27f02cbf088
Arghya-n <sakib.hb7@gmail.com>
2023-06-11 00:17:50 +0600

    Update

--- forum.php
+++ forum.php
@@ -1,62 +1,6 @@
 <?php
 include 'admin/db_connect.php';
 ?>
-<style>
-    #portfolio .img-fluid {
-        width: calc(100%);
-        height: 30vh;
-        z-index: -1;
-        position: relative;
-        padding: 1em;
-    }
-
-    .gallery-list {
-        cursor: pointer;
-        border: unset;
-        flex-direction: inherit;
-    }
-
-    .gallery-img,
-    .gallery-list .card-body {
-        width: calc(50%)
-    }
-
-    .gallery-img img {
-        border-radius: 5px;
-        min-height: 50vh;
-        max-width: calc(100%);
-    }
-
-    span.hightlight {
-        background: yellow;
-    }
-
-    .carousel,
-    .carousel-inner,
-    .carousel-item {
-        min-height: calc(100%)
-    }
-
-    header.masthead,
-    header.masthead:before {
-        min-height: 50vh !important;
-        height: 50vh !important
-    }
-
-    .row-items {
-        position: relative;
-    }
-
-    .masthead {
-        min-height: 23vh !important;
-        height: 23vh !important;
-    }
-
-    .masthead:before {
-        min-height: 23vh !important;
-        height: 23vh !important;
-    }
-</style>
 
 <header class="masthead">
     <div class="container h-150">
